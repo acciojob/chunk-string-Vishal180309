908b14a08934f622af5189b9c2ae6e7ad20b5418
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-02-06 12:38:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function stringChop(str, size) {
-  // your code here
-}
+document.getElementById('chunk-button').addEventListener('click', function() {
+    let inputString = document.getElementById('input-string').value;
+    let chunkLength = parseInt(document.getElementById('chunk-length').value);
+    let result = chunkString(inputString, chunkLength);
 
-// Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+    document.getElementById('result').innerHTML = 'Chunked string: ' + result;
+});
+
+function chunkString(str, chunkLength) {
+    if (str === null) {
+        return [];
+    }
+
+    let chunks = [];
+    for (let i = 0; i < str.length; i += chunkLength) {
+        chunks.push(str.substring(i, Math.min(i + chunkLength, str.length)));
+    }
+
+    return chunks.join(', ');
+}
